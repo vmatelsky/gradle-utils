3eae3f6055de5be5a935f8044f3e406e416848ce
Matelsky, Vladislav <VMatelsky@qulix.com>
2014-10-08 17:44:35 +0300

    add passing parameters to xcodebuild plugin

--- xcodebuildPlugin/plugin/src/main/groovy/com/qulix/xcodebuld/XcodebuildPlugin.groovy
+++ xcodebuildPlugin/plugin/src/main/groovy/com/qulix/xcodebuld/XcodebuildPlugin.groovy
@@ -3,17 +3,11 @@ package com.qulix.xcodebuild
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 
-class XcodebuildPluginExtension {
-    String app_name = 'My_super_gradle_builded_application'
-    String project = "Project.xcodeproj"
-    String scheme = "ProjectScheme"
-    String out_dir = "./build"
-    String configuration = "Release"
-}
+
 
 class XcodebuildPlugin implements Plugin<Project> {
     void apply(Project project) {
-        project.extensions.create('xcodebuild', XcodebuildPluginExtension)
+        project.extensions.create("xcodebuild", XcodebuildPluginExtension)
         project.task('hello', type: GreetingTask)
         project.task('build', type: XcodebuildTask)
     }
--- xcodebuildPlugin/plugin/src/main/groovy/com/qulix/xcodebuld/XcodebuildTask.groovy
+++ xcodebuildPlugin/plugin/src/main/groovy/com/qulix/xcodebuld/XcodebuildTask.groovy
@@ -7,8 +7,23 @@ class XcodebuildTask extends DefaultTask {
 
     @TaskAction
     def build() {
-        println '------------build-------------------'
-        println 'From : ${project.xcodebuild.app_name},\
-                  message : ${project.xcodebuild.project}'
+        println '------------ xcodebuild task -------------------'
+	println "Project: ${project.xcodebuild.project}"
+	println "Scheme: ${project.xcodebuild.scheme}"
+	println "Build dir: ${project.xcodebuild.build_dir}"
+        println "Configuration: ${project.xcodebuild.configuration}"
+	println "ipa path: ${project.xcodebuild.ipa_path}"
+	println "archive path: ${project.xcodebuild.archive_path}"
+	println "provisioning profile: ${project.xcodebuild.provisioning_profile}"
     }
+}
+
+class XcodebuildPluginExtension {
+    String project = "Project.xcodeproj"
+    String scheme = "ProjectScheme"
+    String build_dir = "./build"
+    String configuration = "Release"
+    String ipa_path = "${build_dir}/Project.ipa"
+    String archive_path = "${build_dir}/Project.xcarchive"
+    String provisioning_profile = "TestProvision-dev"
 }
